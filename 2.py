f73efa4838a1a5de58fa6fa2eac070ec9d2d1b05
Liza-040 <04042006liza@gmail.com>
2024-07-29 00:08:30 +0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,5 @@
+homework=12
+hour=1.5
+name_cours="Python"
+hour_1_2_work=float(hour/homework)
+print('Курс:', name_cours+',',"всего задач:", homework,",",'затрачено часов:',hour,",", 'среднее время выполнения', hour_1_2_work, 'часа'+".")
